e78f29c249fff0025bc613d98271d2558be2216d
tk5cdt <30490920+tk5cdt@users.noreply.github.com>
2024-10-21 23:38:59 +0700

    add cors

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import express from 'express'
 import session from 'express-session'
+var cors = require('cors')
 import initApiRoutes from './route/api'
 
 require('dotenv').config();
 let port = process.env.PORT || 8080;
 
 const app = express()
+app.use(cors())
 
 //config to use req.body
 app.use(express.urlencoded({ extended: true }));
